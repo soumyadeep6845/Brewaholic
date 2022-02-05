935400541d0dc717799c90fb36304965d623366d
Soumyadeep Das <soumyadeep.dbsslg@gmail.com>
2022-02-05 20:45:59 +0530

    Setup firebase core in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:brew_app/screens/wrapper.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
