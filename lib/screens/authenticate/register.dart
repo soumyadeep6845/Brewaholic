763933534c55a2c67ce03c556670f01033a14707
Soumyadeep Das <soumyadeep.dbsslg@gmail.com>
2022-02-11 16:54:29 +0530

    Created register form

---
+++ lib/screens/authenticate/register.dart
@@ -0,0 +1,65 @@
+import 'package:brew_app/services/auth.dart';
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  final AuthService _auth = AuthService();
+
+  //Text field state
+  String email = '';
+  String password = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        title: Text('Sign Up to Brewaholic!'),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(
+          vertical: 20.0,
+          horizontal: 50.0,
+        ),
+        child: Form(
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+              TextFormField(
+                onChanged: (val) {
+                  setState(() {
+                    email = val;
+                  });
+                },
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                obscureText: true,
+                onChanged: (val) {
+                  setState(() {
+                    password = val;
+                  });
+                },
+              ),
+              SizedBox(height: 20),
+              RaisedButton(
+                color: Colors.brown[800],
+                child: Text(
+                  'Register',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () async {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
